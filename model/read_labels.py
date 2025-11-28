bb0f7099c357f692675be36620835cf8f182d0f5
piotrh5 <piotrhajduk2002@gmail.com>
2025-11-28 11:28:03 +0100

    add gitignore / data folder

---
+++ model/read_labels.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from pathlib import Path
+
+
+def read_file_labels(path: Path) -> pd.DataFrame:
+    """
+    Type:
+      0: mouse_bite
+      1: spur
+      2: missing_hole
+      3: short
+      4: open_circuit
+      5: spurious_copper
+    """
+    names = ['Type', 'X_max', 'X_min', 'Y_max', 'Y_min', ]
+    df = pd.read_csv(path, sep=' ', names=names)
+    print(df)
+    return df
+
+
+read_file_labels(Path("labels.txt"))
